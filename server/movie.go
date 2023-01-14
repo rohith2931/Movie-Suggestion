4580c95acaa452435fb45d62f0738a59fb02252a
rohith <akularohith89401@gmail.com>
2023-01-14 10:35:37 +0530

    refactor

--- server/movie.go
+++ server/movie.go
@@ -8,20 +8,6 @@ import (
 	"log"
 )
 
-func (s *MsServer) CreateUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
-	log.Printf("creating new user called")
-	newUser := schema.User{
-		UserName:    in.GetUserName(),
-		Password:    in.GetPassword(),
-		Email:       in.GetEmail(),
-		PhoneNumber: in.GetPhoneNumber(),
-		Address:     in.GetAddress(),
-		Watchlist:   &schema.Watchlist{},
-	}
-	s.Db.Create(&newUser)
-	return &pb.User{UserName: in.GetUserName(), Password: in.GetPassword(), Email: in.GetEmail(), PhoneNumber: in.GetPhoneNumber(), Id: uint64(newUser.ID)}, nil
-}
-
 func (s *MsServer) GetAllMovies(in *pb.EmptyMovie, stream pb.MsDatabase_GetAllMoviesServer) error {
 	log.Printf("Getting movies called")
 	Movies := []schema.Movie{}
